37e2f7af5133e20bc287803f74e5fa0377dbcb95
vantheman2210 <martinovic.ba@gmail.com>
2022-01-04 11:38:19 +0100

    Adding dev files and tools

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+
+  
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },  
+  devtool: false,
+  resolve: {
+    extensions: ['.ts', '.js'],
+  }, 
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }, 
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }, 
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }, 
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  }, 
+  
+};
