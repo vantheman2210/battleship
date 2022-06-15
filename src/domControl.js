6fff3a8861665fb8e25ec5c55b5840c6a0edb4a6
vantheman2210 <martinovic.ba@gmail.com>
2022-06-15 11:01:07 +0200

    changed drag and drop function

--- src/domControl.js
+++ src/domControl.js
@@ -1,9 +1,10 @@
-/* eslint-disable no-unused-expressions */ 
+/* eslint-disable no-unused-expressions */
+
 // Selecting elements
-const container = document.querySelector('.container'); 
-const text = document.querySelector('p'); 
-const modal = document.querySelector('.modal');  
-const carrier = document.querySelector('.carrierContainer'); 
+const container = document.querySelector('.container');
+const text = document.querySelector('p');
+const modal = document.querySelector('.modal');
+const carrier = document.querySelector('.carrierContainer');
 const battleship = document.querySelector('.battleshipContainer');
 const cruiser = document.querySelector('.cruiserContainer');
 const submarine = document.querySelector('.submarineContainer');
@@ -18,18 +19,19 @@ const render = (board1, board2) => {
 
 	board1.board.forEach((__a, i) => {
 		const div = document.createElement('div');
-		div.className = 'cells1'; 
-		div.textContent = i; 
-		__a === 'ship' ? div.style.backgroundColor = 'red' : null; 
+		div.className = 'cells1';
+		div.textContent = i;
+		div.dataset.id = i;
+		__a === 'ship' ? (div.style.backgroundColor = 'red') : null;
 		grid1.append(div);
 		container.append(grid1);
 	});
 
 	board2.board.forEach((__a, i) => {
 		const div = document.createElement('div');
-		div.className = 'cells2';  
+		div.className = 'cells2';
 		div.textContent = i;
-		__a === 'ship' ? div.style.backgroundColor = 'red' : null; 
+		__a === 'ship' ? (div.style.backgroundColor = 'red') : null;
 		grid2.append(div);
 		container.append(grid2);
 	});
@@ -40,41 +42,69 @@ const markSpots = (board1, board2) => {
 	const player = document.querySelectorAll('.cells1');
 
 	board1.forEach((element, i) => {
-		
-		element === 'missed' ? (comp[i].style.background = 'gray') : null; 
+		element === 'missed' ? (comp[i].style.background = 'gray') : null;
 		element === 'hit' ? (comp[i].style.background = 'black') : null;
 	});
 
 	board2.forEach((element, i) => {
-		
-		element === 'missed' ? (player[i].style.background = 'gray') : null; 
+		element === 'missed' ? (player[i].style.background = 'gray') : null;
 		element === 'hit' ? (player[i].style.background = 'black') : null;
 	});
-}; 
+};
 
-const showModal = (input) => { 
-	modal.classList.toggle('show-modal'); 
-	container.classList.toggle('is-blurred'); 
-	text.textContent = input;
-}; 
-
-const restart = () => { 
-	container.innerHTML = ''; 
-  modal.classList.toggle('show-modal'); 
+const showModal = (input) => {
+	modal.classList.toggle('show-modal');
 	container.classList.toggle('is-blurred');
-} 
+	text.textContent = input;
+};
 
-const rotate = (e) => { 
-	if(e.target.className === 'carrier')
-	carrier.classList.toggle(`carrierContainer-horizontal`); 
-	if(e.target.className === 'battleship')
-	battleship.classList.toggle(`battleshipContainer-horizontal`); 
-	if(e.target.className === 'cruiser')
-	cruiser.classList.toggle(`cruiserContainer-horizontal`); 
-	if(e.target.className === 'submarine')
-	submarine.classList.toggle(`submarineContainer-horizontal`); 
-	if(e.target.className === 'destroyer')
-	destroyer.classList.toggle(`destroyerContainer-horizontal`);
-}
+const restart = () => {
+	container.innerHTML = '';
+	modal.classList.toggle('show-modal');
+	container.classList.toggle('is-blurred');
+};
 
-export { render, markSpots, showModal, restart, rotate };
+let horizontal = false;
+const rotate = () => {
+	
+	if (horizontal) {
+		carrier.classList.toggle(`carrierContainer-horizontal`);
+
+		battleship.classList.toggle(`battleshipContainer-horizontal`);
+
+		cruiser.classList.toggle(`cruiserContainer-horizontal`);
+		submarine.classList.toggle(`submarineContainer-horizontal`);
+		destroyer.classList.toggle(`destroyerContainer-horizontal`);
+		horizontal = true;
+	}
+	if (!horizontal) {
+		carrier.classList.toggle(`carrierContainer-horizontal`);
+
+		battleship.classList.toggle(`battleshipContainer-horizontal`);
+
+		cruiser.classList.toggle(`cruiserContainer-horizontal`);
+		submarine.classList.toggle(`submarineContainer-horizontal`);
+		destroyer.classList.toggle(`destroyerContainer-horizontal`);
+		horizontal = false;
+	}
+};
+
+const renderModalBoard = () => {
+	// Creating board for placing ships
+	const arr = Array.from({ length: 100 }, (_, i) => i);
+	const board = document.querySelector('.modal-Board');
+	const grid = document.createElement('grid');
+	grid.className = 'grid1';
+
+	arr.forEach((__a, i) => {
+		const div = document.createElement('div');
+		div.className = 'cells1';
+		div.textContent = i;
+		div.dataset.id = i;
+		__a === 'ship' ? (div.style.backgroundColor = 'red') : null;
+		grid.append(div);
+		board.append(grid);
+	});
+};
+
+export { render, markSpots, showModal, restart, rotate, renderModalBoard };
